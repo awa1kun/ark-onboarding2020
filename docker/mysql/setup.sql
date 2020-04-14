c08b5941e0c9f9a90a98cff56e8d0d69bc46b7c8
横川大地 <yk-mac@yokokawadaichinoMacBook-Pro.local>
2020-04-14 11:04:31 +0900

    initial commit

---
+++ docker/mysql/setup.sql
@@ -0,0 +1,20 @@
+/***CREATING ALL TABLES*/
+-- CREATE TABLE EMPLOYEE (
+--   EmployeeId   INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--   FirstName    VARCHAR(40)                    NULL,
+--   LastName     VARCHAR(40)                    NULL,
+--   Phone        VARCHAR(20)                    NULL,
+--   Address      VARCHAR(100)                   NULL,
+--   City         VARCHAR(30)                    NULL,
+--   FullName     VARCHAR(100)                   NULL,
+--   Email        VARCHAR(100)                   NULL,
+--   Pin          VARCHAR(100)                   NULL,
+--   CreationDate DATETIME                       NULL,
+--   EmployeeCode VARCHAR(10) UNIQUE             NOT NULL
+-- )
+
+
+
+
+
+
